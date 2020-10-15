38b6ada4a81531124e9ad830f106e7fb2ec9ecec
Anastasia Saiz <71260261+AnastasiaSaiz@users.noreply.github.com>
2020-10-15 18:49:33 +0200

    modif

--- INDEX.HTML/Buscar.js
+++ INDEX.HTML/Buscar.js
@@ -1,37 +1,22 @@
 function buscarlibro() {
-    let libro = document.getElementById("title").value;
-
+    let libro = document.getElementById("libro").value;
+  ​
     fetch(`https://www.etnassoft.com/api/v1/get/?keyword=${libro}`)
-        .then(function (respuesta) {
-            return respuesta.json
-        })
-        .then(function (datos) {
-            if (!datos.results) {
-                document.getElementById("div1").innerHTML =
-                    "<p>Error en la búsqueda</p>";
-            }
-            document.getElementById("div1").innerHTML = `
-      <h1>${datos.title}</h1>
-      <img src="${datos.cover}" alt="" />
-      <p>${datos.author}</p>
-`;
-            console.log(datos);
-        });
-}
-
-
-
-
-/*
-
-
-           console.log(datos);
-           for (i = 0; i < datos.length; i++) {
-               libro += `<div>
-         <img src=${datos[i].cover} alt=""/>
-         <h1>${datos[i].title} </h1>
-         <h3>${datos[i].author}
-         </div>`;
-           }
-           document.getElementById("books").innerHTML = books;
-       }) */
+      .then(function (respuesta) {
+        return respuesta.json();
+      })
+      .then(function (datos) {
+        if (!datos.results) {
+          document.getElementById("div1").innerHTML =
+            "<p>Error en la búsqueda</p>";
+        }
+        for (let i = 0; i < datos.length; i++) {
+          document.getElementById("div1").innerHTML += `
+        <h1>${datos[i].title}</h1>
+        <img src="${datos[i].cover}" alt="" />
+        <p>${datos[i].author}</p>
+  `;
+        }
+        console.log(datos);
+      });
+  }
