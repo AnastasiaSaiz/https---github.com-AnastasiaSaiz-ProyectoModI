3d00dbb686797631cdc9ec329ec56442d7226955
Anastasia Saiz <71260261+AnastasiaSaiz@users.noreply.github.com>
2020-10-16 11:52:56 +0200

    modif

--- Favoritos.js
+++ Favoritos.js
@@ -1,5 +1,6 @@
 function buscarlibro() {
     let libro = document.getElementById("title").value;
+
     fetch(`https://www.etnassoft.com/api/v1/get/?keyword=${libro}`)
         .then(function (respuesta) {
             return respuesta.json();
@@ -18,4 +19,14 @@ function buscarlibro() {
             }
             console.log(datos);
         });
+        
+    
 }
+let data;
+function mostrar() {
+    data=document.getElementById("title").value;
+    document.getElementById("div1").innerHTML= data; 
+localStorage.setItem("libro", JSON.stringify(title));
+        console.log(JSON.parse(localStorage.getItem("title")));
+    } 
+
